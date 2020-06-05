f652d1e2b983ba5e9d640fe40ba99ac3ea59e69c
Inga Thor <mail@ingathor.com>
2020-06-05 12:30:58 +0200

    Add game phase components

---
+++ src/components/PlayerLobby.vue
@@ -0,0 +1,4 @@
+// Waiting are before game starts // Change of input parameters like: // -
+Number of players wanted for game // - Timer settings for normal, speedy, lazy
+round // Calculated game time based on Number of players * Timer settings //
+Input field for player name
