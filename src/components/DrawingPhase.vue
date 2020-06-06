80a7396d6e4e5665f7541904c904711b99bb2efe
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-06-06 11:59:08 +0200

    display something went wrong in DrawingPhase if there had been no input in WritingPhase

--- src/components/DrawingPhase.vue
+++ src/components/DrawingPhase.vue
@@ -3,7 +3,7 @@
     <h1>Drawing</h1>
     <Timer :timerLengthInMs="8000" ref="timer" @timerfinished="finished" />
 
-    <h2>{{$store.state.currentStage.descriptionTitle}}</h2>
+    <h2>{{$store.state.currentStage.descriptionTitle || "something went wrong"}}</h2>
     <Canvas width="800" height="600" color="green" ref="canvas" />
   </div>
 </template>
