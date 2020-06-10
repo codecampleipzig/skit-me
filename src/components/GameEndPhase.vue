32daec1dac6d84c5dce029c320ac29428bffdb27
WolfgangSiegert <WoSiegert@gmx.de>
2020-06-10 16:09:38 +0200

    Add mock data to show at gameEndphase

--- src/components/GameEndPhase.vue
+++ src/components/GameEndPhase.vue
@@ -3,9 +3,12 @@
     <h1>The End</h1>
 
     <button @click="restartGame">Start a new Game</button>
-    <div v-for="(sheet, index) in $store.state.currentStage.results" :key="index">
+    <div
+      v-for="(sheet, index) in $store.state.currentStage.sheets"
+      :key="index"
+    >
       <div v-for="(result, index) in sheet" :key="index">
-        <img v-if="result.type == 'drawing' " :src="result.content" />
+        <img v-if="result.type == 'drawing'" :src="result.content" />
         <h2 v-else>{{ result.content }}</h2>
       </div>
     </div>
