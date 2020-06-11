dd450cf7bc00db0fe2ec301a054d6140c2495c1e
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-06-11 21:06:21 +0200

    Add clipboard

--- src/components/PlayerLobby.vue
+++ src/components/PlayerLobby.vue
@@ -43,21 +43,8 @@ export default {
       this.$store.dispatch("signalReady");
     },
     copyLink() {
-      /* Get the text field */
-      let copyText = document.querySelector("#linkToRoom");
-      copyText.setAttribute("type", "text");
-      copyText.select();
-      console.log(copyText);
-
-      /* Select the text field */
-      // copyText.select();
-      // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-      // /* Copy the text inside the text field */
-      // document.execCommand("copy");
-
-      // /* Alert the copied text */
-      // alert("Copied the text: " + copyText.value);
+      const copyText = document.querySelector("#linkToRoom");
+      navigator.clipboard.writeText(copyText.textContent);
     }
   }
 };
